dd2828a19d8b9806f28bdac46e5c65b2c72bc6d6
aswad mehmood <aswadmehmood@live.com>
2022-06-17 22:59:28 +0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

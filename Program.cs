aaeef332c84ee2f4e34980635f4591a16e94b89e
aswad mehmood <aswadmehmood@live.com>
2022-06-20 23:28:42 +0500

    Added Regex

--- Program.cs
+++ Program.cs
@@ -1,14 +1,46 @@
-﻿using Models;
+﻿using dotnet_learning.Models;
+using Models;
+using System.Text.RegularExpressions;
 
 namespace dotnet_learning
 {
     internal class Program
     {
+        static public AddressBook addressBook = new AddressBook();
+
         static void Main(string[] args)
         {
-            Address address = new Address();
+            // Get Desktop Path
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = desktopPath + "\\Addresses.txt";
+            string[] fileLines = File.ReadAllLines(filePath);
 
-            
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                if (fileLines[i].ToLower().Contains("sector"))
+                {
+                    City cityAddress = new City();
+                    string[] splitString = fileLines[i].Split(",");
+                    foreach (string addressBit in splitString)
+                    {
+                        if ( addressBit.Contains("House"))
+                        {
+                            string output = Regex.Match(addressBit, @"[0-9]+");
+                            
+                        }
+                        
+                    }
+                    
+                }
+               
+                if (splitString.Count() > 1)
+                {
+                    if (!String.IsNullOrEmpty(splitString[1]))
+                    {
+                        // Do nothing
+                    }
+                }
+            }
         }
     }
 }

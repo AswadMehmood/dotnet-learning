b434b5741b333ea1d4b7fe78b8aca334da93168b
aswad mehmood <aswadmehmood@live.com>
2022-06-22 00:15:13 +0500

    Functions added

--- Program.cs
+++ Program.cs
@@ -10,97 +10,111 @@ namespace dotnet_learning
 
         static void Main(string[] args)
         {
-            // Get Desktop Path
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string filePath = desktopPath + "\\Addresses.txt";
-            string[] fileLines = File.ReadAllLines(filePath);
+
+            string[] fileLines = GetFileLines();
 
             for (int i = 0; i < fileLines.Length; i++)
             {
                 fileLines[i] = fileLines[i].ToLower();
+
                 if (fileLines[i].Contains("sector"))
                 {
-                    City cityAddress = new City();
-                    string[] splitString = fileLines[i].Split(",");
-                    foreach (string addressBit in splitString)
-                    {
-                        if (addressBit.Contains("house"))
-                        {
-                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
-                            cityAddress.houseNumber = Convert.ToInt32(output);
-                        }
-                        else if (addressBit.Contains("street"))
-                        {
-                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
-                            cityAddress.streetNumber = Convert.ToInt32(output);
-                        }
-                        else if (addressBit.Contains("sector"))
-                        {
-                            string output = addressBit.Replace("sector", "").Trim();
-                            cityAddress.sectreName = output;
-                        }
-                        else if (addressBit.Contains("city"))
-                        {
-                            string output = addressBit.Replace("city", "").Trim();
-                            cityAddress.cityName = output;
-                        }
-                        else if (addressBit.Contains("district"))
-                        {
-                            string output = addressBit.Replace("district", "").Trim();
-                            cityAddress.districtName = output;
-                        }
-                        else
-                        {
-                            cityAddress.provinceName = addressBit.Trim();
-                        }
-                    }
-
+                    City cityAddress = ParseCityAddress(fileLines[i]);
                     addressBook.addresses.Add(cityAddress);
-
                 }
                 else
                 {
-                    Village villageAddress = new Village();
-                    string[] splitString = fileLines[i].Split(",");
-                    foreach (string addressBit in splitString)
-                    {
-                        if (addressBit.Contains("house"))
-                        {
-                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
-                            villageAddress.houseNumber = Convert.ToInt32(output);
-                        }
-                        else if (addressBit.Contains("street"))
-                        {
-                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
-                            villageAddress.streetNumber = Convert.ToInt32(output);
-                        }
-                        else if (addressBit.Contains("village"))
-                        {
-                            string output = addressBit.Replace("village", "").Trim();
-                            villageAddress.villageName = output;
-                        }
-                        else if (addressBit.Contains("city"))
-                        {
-                            string output = addressBit.Replace("city", "").Trim();
-                            villageAddress.cityName = output;
-                        }
-                        else if (addressBit.Contains("district"))
-                        {
-                            string output = addressBit.Replace("district", "").Trim();
-                            villageAddress.districtName = output;
-                        }
-                        else
-                        {
-                            villageAddress.provinceName = addressBit.Trim();
-                        }
-
-                    }
-
+                    Village villageAddress = ParseVillageAddress(fileLines[i]);
                     addressBook.addresses.Add(villageAddress);
-
                 }
             }
             Console.WriteLine("print");
+        }
+
+        private static Village ParseVillageAddress(string fileLine)
+        {
+            Village villageAddress = new Village();
+            string[] splitString = fileLine.Split(",");
+            foreach (string addressBit in splitString)
+            {
+                if (addressBit.Contains("house"))
+                {
+                    string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                    villageAddress.houseNumber = Convert.ToInt32(output);
+                }
+                else if (addressBit.Contains("street"))
+                {
+                    string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                    villageAddress.streetNumber = Convert.ToInt32(output);
+                }
+                else if (addressBit.Contains("village"))
+                {
+                    string output = addressBit.Replace("village", "").Trim();
+                    villageAddress.villageName = output;
+                }
+                else if (addressBit.Contains("city"))
+                {
+                    string output = addressBit.Replace("city", "").Trim();
+                    villageAddress.cityName = output;
+                }
+                else if (addressBit.Contains("district"))
+                {
+                    string output = addressBit.Replace("district", "").Trim();
+                    villageAddress.districtName = output;
+                }
+                else
+                {
+                    villageAddress.provinceName = addressBit.Trim();
+                }
+
+            }
+            return villageAddress;
+        }
+
+        private static City ParseCityAddress(string fileLine)
+        {
+            City cityAddress = new City();
+            string[] splitString = fileLine.Split(",");
+            foreach (string addressBit in splitString)
+            {
+                if (addressBit.Contains("house"))
+                {
+                    string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                    cityAddress.houseNumber = Convert.ToInt32(output);
+                }
+                else if (addressBit.Contains("street"))
+                {
+                    string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                    cityAddress.streetNumber = Convert.ToInt32(output);
+                }
+                else if (addressBit.Contains("sector"))
+                {
+                    string output = addressBit.Replace("sector", "").Trim();
+                    cityAddress.sectreName = output;
+                }
+                else if (addressBit.Contains("city"))
+                {
+                    string output = addressBit.Replace("city", "").Trim();
+                    cityAddress.cityName = output;
+                }
+                else if (addressBit.Contains("district"))
+                {
+                    string output = addressBit.Replace("district", "").Trim();
+                    cityAddress.districtName = output;
+                }
+                else
+                {
+                    cityAddress.provinceName = addressBit.Trim();
+                }
+            }
+            return cityAddress;
+        }
+
+        private static string[] GetFileLines()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = desktopPath + "\\Addresses.txt";
+            return File.ReadAllLines(filePath);
         }
     }
 }

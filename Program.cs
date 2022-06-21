e789eb406d7790322fb4478a0af6767db14b9f67
aswad mehmood <aswadmehmood@live.com>
2022-06-21 23:49:05 +0500

    Addressbook implimented

--- Program.cs
+++ Program.cs
@@ -17,30 +17,90 @@ namespace dotnet_learning
 
             for (int i = 0; i < fileLines.Length; i++)
             {
-                if (fileLines[i].ToLower().Contains("sector"))
+                fileLines[i] = fileLines[i].ToLower();
+                if (fileLines[i].Contains("sector"))
                 {
                     City cityAddress = new City();
                     string[] splitString = fileLines[i].Split(",");
                     foreach (string addressBit in splitString)
                     {
-                        if ( addressBit.Contains("House"))
+                        if (addressBit.Contains("house"))
                         {
-                            string output = Regex.Match(addressBit, @"[0-9]+");
-                            
+                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                            cityAddress.houseNumber = Convert.ToInt32(output);
                         }
-                        
+                        else if (addressBit.Contains("street"))
+                        {
+                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                            cityAddress.streetNumber = Convert.ToInt32(output);
+                        }
+                        else if (addressBit.Contains("sector"))
+                        {
+                            string output = addressBit.Replace("sector", "").Trim();
+                            cityAddress.sectreName = output;
+                        }
+                        else if (addressBit.Contains("city"))
+                        {
+                            string output = addressBit.Replace("city", "").Trim();
+                            cityAddress.cityName = output;
+                        }
+                        else if (addressBit.Contains("district"))
+                        {
+                            string output = addressBit.Replace("district", "").Trim();
+                            cityAddress.districtName = output;
+                        }
+                        else
+                        {
+                            cityAddress.provinceName = addressBit.Trim();
+                        }
                     }
-                    
+
+                    addressBook.addresses.Add(cityAddress);
+
                 }
-               
-                if (splitString.Count() > 1)
+                else
                 {
-                    if (!String.IsNullOrEmpty(splitString[1]))
+                    Village villageAddress = new Village();
+                    string[] splitString = fileLines[i].Split(",");
+                    foreach (string addressBit in splitString)
                     {
-                        // Do nothing
+                        if (addressBit.Contains("house"))
+                        {
+                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                            villageAddress.houseNumber = Convert.ToInt32(output);
+                        }
+                        else if (addressBit.Contains("street"))
+                        {
+                            string output = Regex.Match(addressBit, @"[0-9]+").Value.Trim();
+                            villageAddress.streetNumber = Convert.ToInt32(output);
+                        }
+                        else if (addressBit.Contains("village"))
+                        {
+                            string output = addressBit.Replace("village", "").Trim();
+                            villageAddress.villageName = output;
+                        }
+                        else if (addressBit.Contains("city"))
+                        {
+                            string output = addressBit.Replace("city", "").Trim();
+                            villageAddress.cityName = output;
+                        }
+                        else if (addressBit.Contains("district"))
+                        {
+                            string output = addressBit.Replace("district", "").Trim();
+                            villageAddress.districtName = output;
+                        }
+                        else
+                        {
+                            villageAddress.provinceName = addressBit.Trim();
+                        }
+
                     }
+
+                    addressBook.addresses.Add(villageAddress);
+
                 }
             }
+            Console.WriteLine("print");
         }
     }
 }
